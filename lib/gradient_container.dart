21dd7f8f031e0e72694a7f79e2feeef15b1e4181
ariqd <ariqdaffaathallah@gmail.com>
2025-05-22 10:08:44 +0700

    initial commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({
+    super.key,
+    required this.gradientColors,
+  });
+
+  final List<Color> gradientColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text(
+          'Quiz App',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: gradientColors,
+            begin: startAlignment,
+            end: endAlignment,
+          ),
+        ),
+        child: const Center(
+          child: Text('Quiz App'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/gradient_container.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final newTextTheme = Theme.of(context).textTheme.apply(
+          bodyColor: Colors.white,
+          displayColor: Colors.white,
+        );
+
+    return MaterialApp(
+      title: 'Quiz App',
+      theme: ThemeData(primarySwatch: Colors.blue, textTheme: newTextTheme),
+      home: const GradientContainer(
+        gradientColors: [
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98),
+        ],
+      ),
+    );
+  }
+}
